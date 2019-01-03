9f6b3cb97edeac4fe11f1ba53f528a07f25393ec
laci124 <33744907+laci124@users.noreply.github.com>
2019-01-03 15:48:11 +0100

    Merge loop-feature branch

--- src/org/excercise/App.java
+++ src/org/excercise/App.java
@@ -5,6 +5,10 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello.");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Out[" + i + "] = " + i);
+		}
 	}
 
 }
